00d51f2769ef1eb02d40af773aa2ea2779d2ff32
booda86 <booda86@hotmail.it>
2026-08-26 02:58:24 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA9tknn0kwhQtg_OQ9ppURIcEHZSCCCa4Q",
+  authDomain: "haccp-3punto0.firebaseapp.com",
+  projectId: "haccp-3punto0",
+  storageBucket: "haccp-3punto0.firebasestorage.app",
+  messagingSenderId: "440580649694",
+  appId: "1:440580649694:web:d2fbc4eb4b5d8dc6bc884f"
+};
+
+export const WORKSPACE_ID = "3-punto-0";
